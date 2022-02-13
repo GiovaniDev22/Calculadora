87cd0163b566cb688924419bc9981adba6b8e1d6
Giovani Costa Paulo <giovanidev1426@gmail.com>
2022-02-13 17:03:58 -0300

    Primeiro commit

---
+++ script/script.js
@@ -0,0 +1,29 @@
+function calcular(n1, n2) {
+    n1 = parseFloat(document.getElementById("n1").value);
+    n2 = parseFloat(document.getElementById("n2").value);
+
+    selector = document.getElementById("selector").value;
+
+    switch (selector) {
+        case '+':
+            calculo = (n1 + n2)
+            document.getElementById("resul").innerHTML = "O resultado e = " + calculo;
+            break
+        case '-':
+            calculo = (n1 - n2)
+            document.getElementById("resul").innerHTML = "O resultado e = " + calculo;
+            break
+        case '*':
+            calculo = (n1 * n2)
+            document.getElementById("resul").innerHTML = "O resultado e = " + calculo;
+            break
+        case '/':
+            calculo = Math.round((n1 / n2));
+            if (Number.isNaN(calculo)) {
+                document.getElementById("resul").innerHTML = "Insira um divisivel válido"
+            } else {
+                document.getElementById("resul").innerHTML = "O resultado e = " + calculo;
+                break
+            }
+    }
+}
